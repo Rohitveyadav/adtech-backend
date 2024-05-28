1a36f228d90c362e7e6b49757eb34fd49d10ca88
rohitkumar-yadav <rohitkumar.yadav@goodworklabs.com>
2024-05-28 17:12:56 +0530

    db fix

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const userRole =  require('./model/user_roles')
 
 app.use(express.json());
 
+app.get("/", (req,res)=> {
+    res.send("Hello there! Api is working")
+});
+
 app.get('/api/v1/getRoles',userRole.getRoles);
 
 app.post('/api/v1/createRole',userRole.createRoleDb);
