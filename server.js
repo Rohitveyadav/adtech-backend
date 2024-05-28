8633b26b66acc22ac0a969537f63247519b37ec6
rohitkumar-yadav <rohitkumar.yadav@goodworklabs.com>
2024-05-28 17:24:16 +0530

    hot fix

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const app =  require('./app');
 const dotenv = require("dotenv");
-
 dotenv.config();
-
 const PORT = process.env.PORT || 9000;
-
 app.listen(PORT, () => console.log(`Sever is running server file port ${PORT} ...`));
